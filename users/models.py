22a574cd4e296a46156dfb22a886f79ae2a8b278
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-05-31 15:39:07 +0330

    create costum user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from phonenumber_field.modelfields import PhoneNumberField
+from .managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    username = None
+    phone_number = PhoneNumberField(region="IR", unique=True)
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+    objects = CustomUserManager()
+    def __str__(self):
+        return self.phone_number.__str__()
+
+
+
+
