61bce45228edc6c16f133c403b27e2595e363f72
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-01 11:50:35 +0330

    add admin and url address model

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserApiView
-from .views import UserApiView
+from .views import UserApiView, AddressViewSet
 router = DefaultRouter()
 router.register(r'', UserApiView, basename='user-api')
+router.register(r'addresses', AddressViewSet, basename='address')
 urlpatterns = router.urls
