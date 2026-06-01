a4620bfe0338ce34319ee21a3120d39a35d0b4fb
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-01 08:56:02 +0330

    add user to setting and admin pannel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth import get_user_model
 # Register your models here.
+
+class AdminUser(admin.ModelAdmin):
+    fields = ("first_name", "last_name", "phone_number", "email", "last_login")
+admin.site.register(get_user_model(), AdminUser)
