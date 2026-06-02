a44d054414c38c2326b487e3537c408ff723c751
InternetTejarat <kavehrozbah@gmail.com>
2026-06-02 12:16:54 +0330

    define urls views

---
+++ products/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CategoryViewSet, BrandViewSet, ProductAttributeViewSet,
+    ProductViewSet, ReviewViewSet
+)
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'brands', BrandViewSet)
+router.register(r'attributes', ProductAttributeViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'reviews', ReviewViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
