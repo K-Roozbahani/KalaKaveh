ee333b1f20cb79bb9a3336c8b25d7e2615211acc
InternetTejarat <kavehrozbah@gmail.com>
2026-06-05 14:18:35 +0330

    add ProductVarients admin class

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import (
+from products.models import (
     Category, Brand, Product, ProductAttribute,
-    ProductAttributeValue, ProductImage, Review
+    ProductAttributeValue, ProductImage,ProductVariant,
+    ProductVariantAttribute,VariantImage, Review
 )
 
 
@@ -24,6 +25,29 @@ class ProductImageInline(admin.TabularInline):
             return format_html('<img src="{}" style="width: 50px; height: 50px;" />', obj.image.url)
         return "No Image"
 
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
+
+    fields = (
+        "sku",
+        "price",
+        "stock",
+        "is_active",
+    )
+
+    show_change_link = True
+
+class ProductVariantAttributeInline(admin.TabularInline):
+    model = ProductVariantAttribute
+    extra = 1
+    autocomplete_fields = (
+        "attribute_value",
+    )
+
+class VariantImageInline(admin.TabularInline):
+    model = VariantImage
+    extra = 1
 
 class ReviewInline(admin.TabularInline):
     model = Review
@@ -71,6 +95,7 @@ class ProductAdmin(admin.ModelAdmin):
 class ProductAttributeValueAdmin(admin.ModelAdmin):
     list_display = ('product', 'attribute', 'value')
     autocomplete_fields = ('product', 'attribute')
+    search_fields = ('value',)
 
 
 @admin.register(ProductImage)
@@ -82,6 +107,64 @@ class ProductImageAdmin(admin.ModelAdmin):
         if obj.image:
             return format_html('<img src="{}" style="width: 45px; height: 45px;" />', obj.image.url)
         return None
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "product",
+        "sku",
+        "price",
+        "stock",
+        "is_active",
+    )
+
+    list_filter = (
+        "is_active",
+        "product",
+    )
+
+    search_fields = (
+        "sku",
+        "product__name",
+    )
+
+    autocomplete_fields = (
+        "product",
+    )
+
+    inlines = [
+        ProductVariantAttributeInline,
+        VariantImageInline,
+    ]
+
+@admin.register(ProductVariantAttribute)
+class ProductVariantAttributeAdmin(admin.ModelAdmin):
+    list_display = (
+        "variant",
+        "attribute_value",
+    )
+
+    autocomplete_fields = (
+        "variant",
+        "attribute_value",
+    )
+
+@admin.register(VariantImage)
+class VariantImageAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "variant",
+        "is_primary",
+    )
+
+    list_filter = (
+        "is_primary",
+    )
+
+    autocomplete_fields = (
+        "variant",
+    )
 
 
 @admin.register(Review)
