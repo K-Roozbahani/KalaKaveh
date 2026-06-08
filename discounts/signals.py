65d4bcb21519962e6b4b22435f3fb9c0c4ce82e0
InternetTejarat <kavehrozbah@gmail.com>
2026-06-08 08:16:55 +0330

    feat(signals): add discount_deleted discount signal

--- discounts/signals.py
+++ discounts/signals.py
@@ -1,6 +1,7 @@
 # discounts/signals.py
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+
 from django.dispatch import receiver
 
 from discounts.models import Discount
@@ -9,4 +10,8 @@ from discounts.services import refresh_discount_prices
 
 @receiver(post_save, sender=Discount)
 def discount_saved(sender, instance, **kwargs):
+    refresh_discount_prices(instance)
+
+@receiver(post_delete, sender=Discount)
+def discount_deleted(sender, instance, **kwargs):
     refresh_discount_prices(instance)
