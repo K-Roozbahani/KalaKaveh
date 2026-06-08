4e427d64e97ad044d5fddf0d11c9651ef0309c35
InternetTejarat <kavehrozbah@gmail.com>
2026-06-08 15:29:20 +0330

    feat(admins): add search_field on User

--- users/admin.py
+++ users/admin.py
@@ -13,6 +13,11 @@ class AddressInline(admin.TabularInline): # استفاده از TabularInline ب
 class AdminUser(admin.ModelAdmin):
     fields = ("first_name", "last_name", "phone_number", "email", "last_login")
     inlines = (AddressInline,) # اضافه کردن آدرس‌ها به صفحه کاربر
+    search_fields = (
+        "phone_number",
+        "first_name",
+        "last_name",
+    )
 admin.site.register(get_user_model(), AdminUser)
 
 @admin.register(Address)
