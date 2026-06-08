afb89a0c989b504db4e8640877992c7504844e11
InternetTejarat <kavehrozbah@gmail.com>
2026-06-08 21:49:13 +0330

    feat(services): add validate_coupon on carts

--- carts/services/coupon.py
+++ carts/services/coupon.py
@@ -4,6 +4,63 @@ from discounts.models import Coupon
 
 from carts.models import Cart
 
+from rest_framework.exceptions import ValidationError
+
+
+def validate_coupon(*, cart, code):
+    """
+    اعتبارسنجی کد تخفیف.
+    """
+
+    try:
+        coupon = Coupon.objects.select_related("discount").get(
+            code=code,
+        )
+
+    except Coupon.DoesNotExist:
+        raise ValidationError(
+            {
+                "code": "کد تخفیف معتبر نیست."
+            }
+        )
+
+    now = timezone.now()
+
+    discount = coupon.discount
+
+    if not discount.is_active:
+        raise ValidationError(
+            {
+                "code": "کد تخفیف غیرفعال است."
+            }
+        )
+
+    if discount.start_date and discount.start_date > now:
+        raise ValidationError(
+            {
+                "code": "زمان استفاده از این کد تخفیف هنوز آغاز نشده است."
+            }
+        )
+
+    if discount.end_date and discount.end_date < now:
+        raise ValidationError(
+            {
+                "code": "اعتبار کد تخفیف به پایان رسیده است."
+            }
+        )
+
+    if (
+        coupon.usage_limit
+        and coupon.used_count >= coupon.usage_limit
+    ):
+        raise ValidationError(
+            {
+                "code": "ظرفیت استفاده از این کد تخفیف تکمیل شده است."
+            }
+        )
+
+    return coupon
+
 
 def apply_coupon(
     *,
@@ -14,20 +71,11 @@ def apply_coupon(
     اعمال کد تخفیف
     """
 
-    coupon = Coupon.objects.get(
-        code=code
-    )
-
-    # اعتبارسنجی کامل
-    # فعال بودن
-    # تاریخ اعتبار
-    # تعداد استفاده
+    coupon = validate_coupon(code=code)
 
     cart.coupon = coupon
 
-    cart.save(
-        update_fields=["coupon"]
-    )
+    cart.save(update_fields=["coupon"])
 
     return cart
 
@@ -45,3 +93,5 @@ def remove_coupon(*, cart: Cart):
     cart.save(update_fields=["coupon"])
 
     return cart
+
+
