25d47217a33a9d124d44fb45af4a3ddedfd098b7
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-10 12:01:15 +0330

    feat(admins) add addresses admins

--- addresses/admin.py
+++ addresses/admin.py
@@ -1,3 +1,149 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Province, City, Address
+
+
+@admin.register(Province)
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+    ordering = (
+        "name",
+    )
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "province",
+    )
+
+    list_filter = (
+        "province",
+    )
+
+    search_fields = (
+        "name",
+        "province__name",
+    )
+
+    autocomplete_fields = (
+        "province",
+    )
+
+    ordering = (
+        "province",
+        "name",
+    )
+
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "receiver_name",
+        "receiver_phone",
+        "user",
+        "province",
+        "city",
+        "is_default",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_default",
+        "address_type",
+        "province",
+        "city",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "receiver_name",
+        "receiver_phone",
+        "postal_code",
+        "user__phone_number",
+        "user__first_name",
+        "user__last_name",
+    )
+
+    autocomplete_fields = (
+        "user",
+        "province",
+        "city",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            "اطلاعات کاربر",
+            {
+                "fields": (
+                    "user",
+                )
+            },
+        ),
+        (
+            "اطلاعات گیرنده",
+            {
+                "fields": (
+                    "receiver_name",
+                    "receiver_phone",
+                )
+            },
+        ),
+        (
+            "آدرس",
+            {
+                "fields": (
+                    "title",
+                    "address_type",
+                    "province",
+                    "city",
+                    "address_line",
+                    "plaque",
+                    "unit",
+                    "postal_code",
+                    "description",
+                )
+            },
+        ),
+        (
+            "تنظیمات",
+            {
+                "fields": (
+                    "is_default",
+                )
+            },
+        ),
+        (
+            "زمان‌ها",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+
+
+
+
