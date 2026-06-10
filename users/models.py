1b94be08fc9150a610a797c8b8d28adacbfd0689
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-10 11:32:36 +0330

    refactore(app): removed all address classes from users app

--- users/models.py
+++ users/models.py
@@ -1,5 +1,4 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
 from .managers import CustomUserManager
 
@@ -12,32 +11,3 @@ class User(AbstractUser):
     objects = CustomUserManager()
     def __str__(self):
         return self.phone_number.__str__()
-
-class Address(models.Model):
-    # اتصال به کاربر؛ حذف شدن کاربر، آدرس‌های او را هم پاک می‌کند
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
-
-    # جزئیات آدرس
-    title = models.CharField(max_length=50, help_text="عنوان آدرس مثل: خانه یا محل کار")
-    state = models.CharField(max_length=50, help_text="استان")
-    city = models.CharField(max_length=50, help_text="شهر")
-    street = models.CharField(max_length=255, help_text="نام خیابان و کوچه")
-    postal_code = models.CharField(max_length=10, help_text="کد پستی")
-    phone_number = models.CharField(max_length=15, help_text="شماره تماس مرتبط با این آدرس")
-
-    # فیلد برای ذخیره جزئیات بیشتر مثل واحد یا پلاک
-    address_details = models.TextField(blank=True, null=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "آدرس"
-        verbose_name_plural = "آدرس‌ها"
-
-    def __str__(self):
-        return f"{self.title} - {self.city}"
-
-
-
-
