a55d95e40d4fff6ff1197d0a73cb23fe438e9533
InternetTejarat <kavehrozbah@gmail.com>
2026-06-11 11:27:46 +0330

    feat(tests): add factories

---
+++ addresses/tests/factories.py
@@ -0,0 +1,40 @@
+from addresses.models import Province, City, Address
+
+
+def create_province(name="تهران"):
+    return Province.objects.create(name=name)
+
+
+def create_city(province=None, name="تهران"):
+    if province is None:
+        province = create_province()
+
+    return City.objects.create(
+        province=province,
+        name=name,
+    )
+
+
+def create_address(user, **kwargs):
+    province = kwargs.pop("province", None) or create_province()
+    city = kwargs.pop("city", None) or create_city(province=province)
+
+    defaults = {
+        "title": "خانه",
+        "receiver_name": "علی رضایی",
+        "receiver_phone": "+989121111111",
+        "province": province,
+        "city": city,
+        "address_line": "تهران، خیابان آزادی",
+        "plaque": "10",
+        "unit": "2",
+        "postal_code": "1234567890",
+        "is_default": False,
+    }
+
+    defaults.update(kwargs)
+
+    return Address.objects.create(
+        user=user,
+        **defaults,
+    )
