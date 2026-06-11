950525ad4ca0896e48e3bb1ce390b4fd2e414656
InternetTejarat <kavehrozbah@gmail.com>
2026-06-12 01:42:52 +0330

    feat(urls): add jwt urls

--- onlineshop/urls.py
+++ onlineshop/urls.py
@@ -19,8 +19,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls')), # برای لاگین/لاگ‌اوت در مرورگر
     path('api-user/', include('users.api.urls')) ,
     path('api-products/', include('products.api.urls')),
