3d9c938f1d89b4c9a49581d5d40272da43a60f78
InternetTejarat <kavehrozbah@gmail.com>
2026-06-17 16:31:25 +0330

    refactore(tests): add shipment_method to factories on orders

--- orders/tests/factories.py
+++ orders/tests/factories.py
@@ -24,17 +24,52 @@ def create_user(**kwargs):
     )
 
 
+def create_shipping_snapshot():
+    return {
+        "id": 1,
+        "name": "پست پیشتاز",
+        "price": "80000",
+        "estimated_days": 3,
+    }
+
+
+
 def create_order(user=None, **kwargs):
     if user is None:
         user = create_user()
 
     return Order.objects.create(
         user=user,
-        order_number=kwargs.get("order_number", generate_order_number()),
-        status=kwargs.get("status", OrderStatus.PENDING),
-        address_snapshot=kwargs.get("address_snapshot", {}),
-        subtotal=kwargs.get("subtotal", 100000),
-        discount_amount=kwargs.get("discount_amount", 0),
-        shipping_cost=kwargs.get("shipping_cost", 0),
-        total_amount=kwargs.get("total_amount", 100000),
+        order_number=kwargs.get(
+            "order_number",
+            generate_order_number(),
+        ),
+        status=kwargs.get(
+            "status",
+            OrderStatus.PENDING,
+        ),
+        address_snapshot=kwargs.get(
+            "address_snapshot",
+            {},
+        ),
+        shipping_method_snapshot=kwargs.get(
+            "shipping_method_snapshot",
+            create_shipping_snapshot(),
+        ),
+        subtotal=kwargs.get(
+            "subtotal",
+            100000,
+        ),
+        discount_amount=kwargs.get(
+            "discount_amount",
+            0,
+        ),
+        shipping_cost=kwargs.get(
+            "shipping_cost",
+            0,
+        ),
+        total_amount=kwargs.get(
+            "total_amount",
+            100000,
+        ),
     )
