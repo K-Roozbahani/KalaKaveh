f82f924a7156996bfbfdd53ae3983e8b2ed955d8
InternetTejarat <kavehrozbah@gmail.com>
2026-06-17 09:38:59 +0330

    feat(test): add test_models on shipping

---
+++ shipping/tests/test_models.py
@@ -0,0 +1,125 @@
+from django.test import TestCase
+
+from shipping.constants import ShipmentStatus
+
+from shipping.tests.factories import (
+    create_shipment,
+    create_shipping_method,
+)
+
+from orders.tests.factories import (
+    create_order,
+)
+
+
+class ShippingMethodModelTest(TestCase):
+    def test_create_shipping_method(self):
+        shipping_method = create_shipping_method()
+
+        self.assertEqual(
+            shipping_method.name,
+            "پست پیشتاز",
+        )
+
+        self.assertTrue(
+            shipping_method.is_active,
+        )
+
+    def test_shipping_method_str(self):
+        shipping_method = create_shipping_method(
+            name="تیپاکس",
+        )
+
+        self.assertEqual(
+            str(shipping_method),
+            "تیپاکس",
+        )
+
+
+class ShipmentModelTest(TestCase):
+    def test_create_shipment(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertEqual(
+            shipment.order,
+            order,
+        )
+
+        self.assertEqual(
+            shipment.status,
+            ShipmentStatus.PENDING,
+        )
+
+    def test_default_status_is_pending(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertEqual(
+            shipment.status,
+            ShipmentStatus.PENDING,
+        )
+
+    def test_tracking_code_default_is_empty(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertEqual(
+            shipment.tracking_code,
+            "",
+        )
+
+    def test_shipped_at_default_is_none(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertIsNone(
+            shipment.shipped_at,
+        )
+
+    def test_delivered_at_default_is_none(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertIsNone(
+            shipment.delivered_at,
+        )
+
+    def test_description_default_is_empty(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertEqual(
+            shipment.description,
+            "",
+        )
+
+    def test_shipment_str(self):
+        order = create_order()
+
+        shipment = create_shipment(
+            order=order,
+        )
+
+        self.assertEqual(
+            str(shipment),
+            f"Shipment - {self.order.order_number}"
+        )
