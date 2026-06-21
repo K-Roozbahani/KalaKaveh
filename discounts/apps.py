34d9bdf30aaf4742079ac8baab3fe84a99c6fa15
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-21 13:02:54 +0330

    refactor(app): remove signals from Discounts app

--- discounts/apps.py
+++ discounts/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class DiscountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'discounts'
-
-    def ready(self):
-        import discounts.signals
