7e183627e2a2aa07ab397cbbdb56e34abdebe220
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-21 16:58:01 +0330

    feat(settings): add celery and redis config

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -151,5 +151,31 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
-# ____________________________________________________________________________
+# _____________________________Cache redis____________________________________________
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "shop",
+    }
+}
+
+# ______________________________Celery___________________________________________________
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+
+CELERY_ACCEPT_CONTENT = ["json"]
+
+CELERY_TASK_SERIALIZER = "json"
+
+CELERY_RESULT_SERIALIZER = "json"
+
+CELERY_TIMEZONE = "Asia/Tehran"
+
 
