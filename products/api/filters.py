2ff11f8b3bdf450cf5000819e26bb709a1553f36
InternetTejarat <kavehrozbah@gmail.com>
2026-06-23 17:15:38 +0330

    feat(filter): improve products filters

--- products/api/filters.py
+++ products/api/filters.py
@@ -8,13 +8,33 @@ class ProductFilter(django_filters.FilterSet):
     """
 
     name = django_filters.CharFilter(lookup_expr="icontains")
-    category = django_filters.NumberFilter()
-    brand = django_filters.NumberFilter()
+    category = django_filters.CharFilter(
+        field_name="category__slug",
+    )
+
+    brand = django_filters.CharFilter(
+        field_name="brand__slug",
+    )
+
+    slug = django_filters.CharFilter(
+        lookup_expr="icontains",
+    )
+
+    created_after = django_filters.DateTimeFilter(
+        field_name="created_at",
+        lookup_expr="gte",
+    )
+
+    created_before = django_filters.DateTimeFilter(
+        field_name="created_at",
+        lookup_expr="lte",
+    )
+
     is_active = django_filters.BooleanFilter()
 
     class Meta:
         model = Product
-        fields = ["name", "category", "brand", "is_active"]
+        fields = ["name", "category", "brand", "is_active", "slug", "created_after", "created_before"]
 
 
 class VariantFilter(django_filters.FilterSet):
@@ -22,13 +42,22 @@ class VariantFilter(django_filters.FilterSet):
     فیلتر واریانت‌ها
     """
 
-    price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
-    price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+    price_min = django_filters.NumberFilter(
+        field_name="final_price",
+        lookup_expr="gte",
+    )
+
+    price_max = django_filters.NumberFilter(
+        field_name="final_price",
+        lookup_expr="lte",
+    )
+
+    is_active = django_filters.BooleanFilter()
     in_stock = django_filters.BooleanFilter(method="filter_in_stock")
 
     class Meta:
         model = ProductVariant
-        fields = ["price_min", "price_max"]
+        fields = ["price_min", "price_max", "is_active", "in_stock"]
 
     def filter_in_stock(self, queryset, name, value):
         if value:
