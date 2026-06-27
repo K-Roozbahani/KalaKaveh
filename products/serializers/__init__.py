5d77ad39cf49a2d027b605474e1457db70ca069a
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-27 15:01:10 +0330

    feat(serializers): update __init__ products

--- products/serializers/__init__.py
+++ products/serializers/__init__.py
@@ -4,3 +4,7 @@ from .image import (
     ProductImageSerializer,
     VariantImageSerializer,
 )
+from .variant import VariantSerializer
+from .review import ReviewSerializer
+from .product_list import ProductListSerializer
+from .product_detail import ProductDetailSerializer
