54f0ccab1e3f311952a9fb2ecef240d6d6223160
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-27 14:54:21 +0330

    faet(serializers): add image

---
+++ products/serializers/image.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from products.models import (
+    ProductImage,
+    VariantImage,
+)
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    """
+    تصاویر محصول
+    """
+
+    class Meta:
+        model = ProductImage
+
+        fields = (
+            "id",
+            "image",
+            "alt_text",
+        )
+
+        read_only_fields = fields
+
+
+class VariantImageSerializer(serializers.ModelSerializer):
+    """
+    تصاویر تنوع محصول
+    """
+
+    class Meta:
+        model = VariantImage
+
+        fields = (
+            "id",
+            "image",
+            "is_primary",
+        )
+
+        read_only_fields = fields
