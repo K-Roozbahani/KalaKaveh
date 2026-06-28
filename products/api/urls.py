8e71dd8123dc243c234bb5bb83e5bd61c1c2825d
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-28 08:48:53 +0330

    reafctor(urls): remove extra urls on products urls

--- products/api/urls.py
+++ products/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.api.view import (
-    CategoryViewSet, BrandViewSet, ProductAttributeViewSet,
-    ProductViewSet, ReviewViewSet
-)
+from products.api.views.product import ProductViewSet
+
 
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'brands', BrandViewSet)
-router.register(r'attributes', ProductAttributeViewSet)
-router.register(r'products', ProductViewSet)
-router.register(r'reviews', ReviewViewSet)
+# router.register(r'categories', CategoryViewSet)
+# router.register(r'brands', BrandViewSet)
+# router.register(r'attributes', ProductAttributeViewSet)
+# router.register(r'products', ProductViewSet)
+# router.register(r'reviews', ReviewViewSet)
+
+router.register(r'products', ProductViewSet, basename="product")
 
 urlpatterns = router.urls
 
