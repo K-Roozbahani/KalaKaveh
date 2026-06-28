e7acabd3059bbdb66993577881997bd626e12c86
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-28 11:40:42 +0330

    feat(urls): add review

--- products/api/urls.py
+++ products/api/urls.py
@@ -4,12 +4,12 @@ from rest_framework.routers import DefaultRouter
 from products.api.views.brand import BrandViewSet
 from products.api.views.category import CategoryViewSet
 from products.api.views.product import ProductViewSet
-
+from products.api.views.review import ReviewViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename="category")
 router.register(r'brands', BrandViewSet, basename="brand")
-# router.register(r'reviews', ReviewViewSet)
+router.register(r'reviews', ReviewViewSet, basename="review" )
 
 router.register(r'products', ProductViewSet, basename="product")
 
