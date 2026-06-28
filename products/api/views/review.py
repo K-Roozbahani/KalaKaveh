46ca254734ec210d1bbb4cb2d4dfa8656cc11550
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-28 11:58:25 +0330

    recaftor(views): add permission to review

--- products/api/views/review.py
+++ products/api/views/review.py
@@ -1,22 +1,24 @@
 from rest_framework import status
 from rest_framework.mixins import (
     CreateModelMixin,
-    DestroyModelMixin,
     ListModelMixin,
     RetrieveModelMixin,
     UpdateModelMixin,
 )
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from products.api.serializers import (
+from core.permissions import IsOwnerOrAdmin
+
+from products.api.serializers.review import (
     ReviewSerializer,
     ReviewWriteSerializer,
 )
 from products.selectors import (
+    get_product_by_id,
     get_product_reviews,
     get_review_by_id,
-    get_product_by_id,
 )
 from products.services.review import (
     create_review,
@@ -30,31 +32,64 @@ class ReviewViewSet(
     CreateModelMixin,
     RetrieveModelMixin,
     UpdateModelMixin,
-    DestroyModelMixin,
     GenericViewSet,
 ):
     """
-    API نظرات محصولات
+    مدیریت نظرات محصولات
     """
 
-    lookup_field = "pk"
+    def get_permissions(self):
+        """
+        تعیین Permission بر اساس Action
+        """
+
+        if self.action in (
+            "list",
+            "retrieve",
+        ):
+            permission_classes = [
+                AllowAny,
+            ]
+
+        elif self.action == "create":
+            permission_classes = [
+                IsAuthenticated,
+            ]
+
+        else:
+            permission_classes = [
+                IsOwnerOrAdmin,
+            ]
+
+        return [
+            permission()
+            for permission in permission_classes
+        ]
 
     def get_queryset(self):
         """
-        لیست نظرات یک محصول
+        لیست نظرات تایید شده محصول
         """
 
-        if "product_pk" in self.kwargs:
-            return get_product_reviews(
-                product_id=self.kwargs["product_pk"],
-            )
-
-        return get_product_reviews()
+        return get_product_reviews(
+            product_id=self.kwargs["product_pk"],
+        )
 
     def get_object(self):
-        return get_review_by_id(
+        """
+        دریافت یک نظر
+        """
+
+        obj = get_review_by_id(
             review_id=self.kwargs["pk"],
         )
+
+        self.check_object_permissions(
+            self.request,
+            obj,
+        )
+
+        return obj
 
     def get_serializer_class(self):
 
@@ -92,14 +127,37 @@ class ReviewViewSet(
             status=status.HTTP_201_CREATED,
         )
 
-    def perform_update(self, serializer):
+    def update(self, request, *args, **kwargs):
 
-        update_review(
-            review=self.get_object(),
+        review = self.get_object()
+
+        serializer = self.get_serializer(
+            review,
+            data=request.data,
+            partial=kwargs.get(
+                "partial",
+                False,
+            ),
+        )
+
+        serializer.is_valid(
+            raise_exception=True,
+        )
+
+        review = update_review(
+            review=review,
             **serializer.validated_data,
         )
 
+        return Response(
+            ReviewSerializer(review).data,
+        )
+
     def destroy(self, request, *args, **kwargs):
+        """
+        حذف منطقی نظر
+        """
+
         review = self.get_object()
 
         deactivate_review(
