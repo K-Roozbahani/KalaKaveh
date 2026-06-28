5ded7d4759d36735cdba08a3fd8d3476eb67275a
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-06-28 09:15:26 +0330

    feat(urls): add catagory and brand urls

--- products/api/urls.py
+++ products/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from products.api.views.brand import BrandViewSet
+from products.api.views.category import CategoryViewSet
 from products.api.views.product import ProductViewSet
 
 
 router = DefaultRouter()
-# router.register(r'categories', CategoryViewSet)
-# router.register(r'brands', BrandViewSet)
-# router.register(r'attributes', ProductAttributeViewSet)
-# router.register(r'products', ProductViewSet)
+router.register(r'categories', CategoryViewSet, basename="category")
+router.register(r'brands', BrandViewSet, basename="brand")
 # router.register(r'reviews', ReviewViewSet)
 
 router.register(r'products', ProductViewSet, basename="product")
