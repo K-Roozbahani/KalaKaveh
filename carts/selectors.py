ffa2bc8e5bb0ea6d239c1b91cc0202027354d83d
InternetTejarat <kavehrozbah@gmail.com>
2026-06-29 17:17:50 +0330

    feat(selectors): add get_cart_item_by_id on carts

--- carts/selectors.py
+++ carts/selectors.py
@@ -53,3 +53,59 @@ def get_guest_active_cart(session_key):
         )
         .first()
     )
+
+
+from carts.constants import CartStatus
+from carts.models import CartItem
+
+
+def get_cart_item_queryset():
+    """
+    QuerySet بهینه برای آیتم‌های سبد خرید
+    """
+
+    return (
+        CartItem.objects.select_related(
+            "cart",
+            "variant",
+            "variant__product",
+            "variant__product__brand",
+            "variant__product__category",
+        )
+    )
+
+
+def get_cart_item_by_id(
+    *,
+    item_id,
+    user=None,
+    session_key=None,
+):
+    """
+    دریافت آیتم سبد خرید بر اساس شناسه
+
+    فقط در صورتی آیتم برگردانده می‌شود که
+    متعلق به سبد خرید فعال کاربر یا مهمان باشد.
+    """
+
+    queryset = get_cart_item_queryset().filter(
+        id=item_id,
+        cart__status=CartStatus.ACTIVE,
+    )
+
+    if user is not None:
+
+        queryset = queryset.filter(
+            cart__user=user,
+        )
+
+    elif session_key is not None:
+
+        queryset = queryset.filter(
+            cart__session_key=session_key,
+        )
+
+    else:
+        return None
+
+    return queryset.first()
