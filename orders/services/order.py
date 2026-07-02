cb640e2fc01c967453dade1c9025f80cce43a8c9
InternetTejarat <kavehrozbah@gmail.com>
2026-07-02 19:05:52 +0330

    refactor(services): convert shipping servives to package

--- orders/services/order.py
+++ orders/services/order.py
@@ -13,6 +13,7 @@ from carts.services.pricing import (
     calculate_cart_totals,
 )
 from discounts.models import Coupon
+from discounts.services.coupon import validate_coupon
 
 from orders.models import (
     Order,
@@ -64,6 +65,12 @@ def create_order_from_cart(
     validate_cart_not_empty(cart)
     validate_cart_stock(cart)
 
+    if not coupon is None:
+        validate_coupon(
+            user=user,
+            coupon=coupon,
+        )
+
     address = get_address_by_id(
         address_id=address_id,
     )
@@ -100,6 +107,7 @@ def create_order_from_cart(
         address_snapshot=build_address_snapshot(
             address
         ),
+        coupon= coupon,
 
         subtotal=totals["subtotal"],
         discount_amount=totals["discount"],
