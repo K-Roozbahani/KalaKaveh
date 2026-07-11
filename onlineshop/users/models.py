881b3f0b2a49366d2730164e7c565bcdba444b7a
InternetTejarat <kavehrozbah@gmail.com>
2026-07-11 15:13:30 +0330

    feat(models): add Blacklist

--- onlineshop/users/models.py
+++ onlineshop/users/models.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 from phonenumber_field.modelfields import PhoneNumberField
 from .managers import CustomUserManager
 
@@ -11,3 +14,50 @@ class User(AbstractUser):
     objects = CustomUserManager()
     def __str__(self):
         return self.phone_number.__str__()
+
+
+class Blacklist(models.Model):
+    """
+    لیست سیاه برای جلوگیری از ارسال OTP و درخواست‌های نامعتبر.
+    """
+
+    phone_number = PhoneNumberField(
+        region="IR",
+        blank=True,
+        null=True,
+        verbose_name=_("شماره موبایل"),
+    )
+
+    ip_address = models.GenericIPAddressField(
+        blank=True,
+        null=True,
+        verbose_name=_("آدرس IP"),
+    )
+
+    reason = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name=_("دلیل"),
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("فعال"),
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("زمان ایجاد"),
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("زمان بروزرسانی"),
+    )
+
+    class Meta:
+        verbose_name = _("لیست سیاه")
+        verbose_name_plural = _("لیست سیاه")
+
+    def __str__(self):
+        return self.phone_number or self.ip_address
