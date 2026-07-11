24a018e15f3e636e1eaa572e572ecc9d9b9a7119
InternetTejarat <kavehrozbah@gmail.com>
2026-07-11 15:37:22 +0330

    feat(validators): add onlineshop/users/validators.py

--- onlineshop/users/validators.py
+++ onlineshop/users/validators.py
@@ -1,9 +1,75 @@
-validate_phone_number(phone_number)
+from phonenumber_field.phonenumber import PhoneNumber
 
-validate_request_source(
-    phone_number=None,
-    ip_address=None,
-    user=None,
+from users.selectors import (
+    get_blacklisted_ip,
+    get_blacklisted_phone,
 )
 
-validate_otp(otp)
+from users.exceptions import (
+    InvalidOTPException,
+    PhoneNumberBlockedException,
+    IPAddressBlockedException,
+)
+
+
+# ===========================================================
+# Phone Number Validators
+# ===========================================================
+
+def validate_phone_blacklist(
+    phone_number: PhoneNumber,
+) -> None:
+    """
+    بررسی بلاک بودن شماره موبایل.
+
+    Args:
+        phone_number: شماره موبایل.
+
+    Raises:
+        PhoneNumberBlockedException: در صورت بلاک بودن شماره.
+    """
+    if get_blacklisted_phone(phone_number):
+        raise PhoneNumberBlockedException
+
+
+# ===========================================================
+# IP Address Validators
+# ===========================================================
+
+def validate_ip_blacklist(
+    ip_address: str,
+) -> None:
+    """
+    بررسی بلاک بودن آدرس IP.
+
+    Args:
+        ip_address: آدرس IP.
+
+    Raises:
+        IPAddressBlockedException: در صورت بلاک بودن IP.
+    """
+    if get_blacklisted_ip(ip_address):
+        raise IPAddressBlockedException
+
+
+# ===========================================================
+# OTP Validators
+# ===========================================================
+
+def validate_otp(
+    otp: str,
+) -> None:
+    """
+    اعتبارسنجی فرمت کد یکبار مصرف.
+
+    Args:
+        otp: کد یکبار مصرف.
+
+    Raises:
+        InvalidOTPException: در صورت نامعتبر بودن فرمت کد.
+    """
+    if not otp.isdigit():
+        raise InvalidOTPException
+
+    if len(otp) != 6:
+        raise InvalidOTPException
