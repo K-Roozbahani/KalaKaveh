ed6e6e114b3c7c2b08ab8eef0b39965a7a3aecb9
InternetTejarat <kavehrozbah@gmail.com>
2026-07-12 11:52:51 +0330

    feat(urls): add authentications urls

--- onlineshop/users/api/urls.py
+++ onlineshop/users/api/urls.py
@@ -1,5 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserApiView
+from .views import UserApiView, AuthenticationViewSet
+
+
 router = DefaultRouter()
-router.register(r'', UserApiView, basename='user-api')
+
+router.register("auth", AuthenticationViewSet, basename="authentication")
+
+router.register('', UserApiView, basename='user')
+
 urlpatterns = router.urls
