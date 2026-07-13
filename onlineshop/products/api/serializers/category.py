e3a3fe92116ace8d1d0e015e3da2e7ef84907f48
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-07-13 13:18:53 +0330

    refactor(serializers): update category parent

--- onlineshop/products/api/serializers/category.py
+++ onlineshop/products/api/serializers/category.py
@@ -7,6 +7,7 @@ class CategorySerializer(serializers.ModelSerializer):
     """
     نمایش اطلاعات دسته‌بندی
     """
+    parent = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
@@ -19,3 +20,14 @@ class CategorySerializer(serializers.ModelSerializer):
         )
 
         read_only_fields = fields
+
+    def get_parent(self, obj):
+        if not obj.parent:
+            return None
+
+        return {
+            "id": obj.parent.id,
+            "name": obj.parent.name,
+            "slug": obj.parent.slug,
+            "parent": self.get_parent(obj.parent),
+        }
