d63418fb9e0b66b702ddf14f8e79a96d5e83a645
InternetTejarat <kavehrozbah@gmail.com>
2026-07-14 12:02:39 +0330

    refactor(serializers): add path and rm parent to CategorySerializers

--- onlineshop/products/api/serializers/category.py
+++ onlineshop/products/api/serializers/category.py
@@ -7,7 +7,8 @@ class CategorySerializer(serializers.ModelSerializer):
     """
     نمایش اطلاعات دسته‌بندی
     """
-    parent = serializers.SerializerMethodField()
+
+    path = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
@@ -16,18 +17,28 @@ class CategorySerializer(serializers.ModelSerializer):
             "id",
             "name",
             "slug",
-            "parent",
+            "path",
         )
 
         read_only_fields = fields
 
-    def get_parent(self, obj):
-        if not obj.parent:
-            return None
+    # def get_parent(self, obj):
+    #     if not obj.parent:
+    #         return None
+    #
+    #     return {
+    #         "id": obj.parent.id,
+    #         "name": obj.parent.name,
+    #         "slug": obj.parent.slug,
+    #         "parent": self.get_parent(obj.parent),
+    #     }
 
-        return {
-            "id": obj.parent.id,
-            "name": obj.parent.name,
-            "slug": obj.parent.slug,
-            "parent": self.get_parent(obj.parent),
-        }
+    def get_path(self, obj):
+        path = []
+
+        current = obj
+        while current is not None:
+            path.append(current.slug)
+            current = current.parent
+
+        return list(reversed(path))
