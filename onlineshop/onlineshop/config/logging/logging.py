26a27652adeef880bce6742bd143b9bd0fdd2a09
InternetTejarat <kavehrozbah@gmail.com>
2026-07-19 07:31:45 +0330

    fix(logging): path loging formater

--- onlineshop/onlineshop/config/logging/logging.py
+++ onlineshop/onlineshop/config/logging/logging.py
@@ -90,7 +90,7 @@ LOGGING = {
 
     "formatters": {
         "standard": {
-            "()": "config.logging.formatters.ProductionFormatter",
+            "()": "onlineshop.config.logging.formatters.ProductionFormatter",
             "format": (
                 "[%(asctime)s] "
                 "%(levelname)-8s "
@@ -99,7 +99,7 @@ LOGGING = {
             "datefmt": "%Y-%m-%d %H:%M:%S",
         },
         "error": {
-            "()": "config.logging.formatters.ProductionFormatter",
+            "()": "onlineshop.config.logging.formatters.ProductionFormatter",
             "format": (
                 "\n"
                 "================================================================================\n"
