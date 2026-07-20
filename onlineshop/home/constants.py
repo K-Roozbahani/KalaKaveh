c9533853ca959feec3edb34efff558f8626909b0
InternetTejarat <kavehrozbah@gmail.com>
2026-07-20 10:59:34 +0330

    feat(constants): add HomeSectionType on home

---
+++ onlineshop/home/constants.py
@@ -0,0 +1,28 @@
+# home/constants.py
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class HomeSectionType(models.TextChoices):
+    """
+    انواع سکشن‌های صفحه اصلی
+    """
+
+    HERO_SLIDER = "hero_slider", _("اسلایدر اصلی")
+
+    BANNER = "banner", _("بنر")
+
+    CATEGORIES = "categories", _("دسته‌بندی‌ها")
+
+    FEATURED_PRODUCTS = "featured_products", _("محصولات ویژه")
+
+    LATEST_PRODUCTS = "latest_products", _("جدیدترین محصولات")
+
+    BEST_SELLER_PRODUCTS = "best_seller_products", _("پرفروش‌ترین محصولات")
+
+    DISCOUNT_PRODUCTS = "discount_products", _("محصولات تخفیف‌دار")
+
+    BRANDS = "brands", _("برندها")
+
+    CUSTOM_PRODUCTS = "custom_products", _("محصولات انتخابی")
