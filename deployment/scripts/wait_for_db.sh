edd466e5f6cbe5c3e3958f3def124f8bfd54a49c
InternetTejarat <kavehrozbah@gmail.com>
2026-07-24 02:00:55 +0330

    refactor(scripts): add python command for test postgrac

--- deployment/scripts/wait_for_db.sh
+++ deployment/scripts/wait_for_db.sh
@@ -4,11 +4,12 @@ set -e
 
 echo "Waiting for PostgreSQL..."
 
-until pg_isready \
-    -h "$DB_HOST" \
-    -p "$DB_PORT" \
-    -U "$DB_USER"
+until python manage.py shell -c "
+from django.db import connection
+connection.ensure_connection()
+" >/dev/null 2>&1
 do
+    echo "Database unavailable - sleeping..."
     sleep 1
 done
 
