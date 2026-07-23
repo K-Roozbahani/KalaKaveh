3651af1cc21226febf48ddd9db134927977d4203
InternetTejarat <kavehrozbah@gmail.com>
2026-07-23 11:29:08 +0330

    refactor(servcies): change output style on home

--- onlineshop/home/api/serializers.py
+++ onlineshop/home/api/serializers.py
@@ -4,9 +4,9 @@ from home.constants import HomeSectionType
 from home.models import Banner, HeroSlide
 
 from products.api.serializers import (
-    ProductListSerializer,
     BrandSerializer,
-    CategorySerializer
+    CategorySerializer,
+    ProductListSerializer,
 )
 
 
@@ -39,6 +39,14 @@ class BannerSerializer(serializers.ModelSerializer):
 
 
 class HomeSectionSerializer(serializers.Serializer):
+    """
+    Serializer مربوط به هر سکشن صفحه اصلی.
+    """
+
+    SECTION_SERIALIZERS = {
+        HomeSectionType.HERO_SLIDER: HeroSlideSerializer,
+        HomeSectionType.BANNER: BannerSerializer,
+    }
 
     title = serializers.CharField(
         source="section.title",
@@ -55,44 +63,59 @@ class HomeSectionSerializer(serializers.Serializer):
     items = serializers.SerializerMethodField()
 
     def get_items(self, obj):
+        """
+        سریالایز آیتم‌های هر سکشن.
+        """
 
         section = obj["section"]
-
         items = obj["items"]
 
-        match section.section_type:
+        serializer_class = self.SECTION_SERIALIZERS.get(
+            section.section_type,
+        )
 
-            case HomeSectionType.HERO_SLIDER:
-                return HeroSlideSerializer(
-                    items,
-                    many=True,
-                    context=self.context,
-                ).data
+        if serializer_class is not None:
+            return serializer_class(
+                items,
+                many=True,
+                context=self.context,
+            ).data
 
-            case HomeSectionType.BANNER:
-                return BannerSerializer(
-                    items,
-                    many=True,
-                    context=self.context,
-                ).data
+        if section.section_type == HomeSectionType.CATEGORIES:
+            return CategorySerializer(
+                [item.category for item in items],
+                many=True,
+                context=self.context,
+            ).data
 
-            case HomeSectionType.CATEGORIES:
-                return CategorySerializer(
-                    [item.category for item in items],
-                    many=True,
-                    context=self.context,
-                ).data
+        if section.section_type == HomeSectionType.BRANDS:
+            return BrandSerializer(
+                [item.brand for item in items],
+                many=True,
+                context=self.context,
+            ).data
 
-            case HomeSectionType.BRANDS:
-                return BrandSerializer(
-                    [item.brand for item in items],
-                    many=True,
-                    context=self.context,
-                ).data
+        return ProductListSerializer(
+            items,
+            many=True,
+            context=self.context,
+        ).data
 
-            case _:
-                return ProductListSerializer(
-                    items,
-                    many=True,
-                    context=self.context,
-                ).data
+class HomePageSerializer(serializers.Serializer):
+    """
+    Serializer خروجی صفحه اصلی.
+    """
+
+    def to_representation(self, instance):
+
+        data = {}
+
+        for section_type, sections in instance.items():
+
+            data[section_type] = HomeSectionSerializer(
+                sections,
+                many=True,
+                context=self.context,
+            ).data
+
+        return data
