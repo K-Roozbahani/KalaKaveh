71b3776eeef9e0c55728dda8113a0b67daef3b0e
InternetTejarat <kavehrozbah@gmail.com>
2026-07-29 12:25:58 +0330

    feat(logging): add celery loggings

--- onlineshop/onlineshop/config/logging/logging.py
+++ onlineshop/onlineshop/config/logging/logging.py
@@ -17,7 +17,7 @@ env.read_env(BASE_DIR / ".env")
 # Logging Environment Variables
 # ------------------------------------------------------------------
 
-LOG_DIR = Path(env("LOG_DIR", default=BASE_DIR / "logs"))
+LOG_DIR = Path(env("LOG_DIR", default=BASE_DIR / "logs/django"))
 LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 LOG_LEVEL = env("LOG_LEVEL", default="INFO")
@@ -59,6 +59,38 @@ handlers = {
         "encoding": "utf-8",
         "delay": True,
         "formatter": "error",
+    },
+    # ------------------------------------------------------------------
+    # Celery Worker Handler
+    # ------------------------------------------------------------------
+
+    # ------------------------------------------------------------------
+    # Celery Worker Handler
+    # ------------------------------------------------------------------
+
+    "celery_worker_file": {
+        "level": "INFO",
+        "class": "logging.handlers.TimedRotatingFileHandler",
+        "filename": LOG_DIR / "celery" / "worker.log",
+        "when": "midnight",
+        "interval": 1,
+        "backupCount": 30,
+        "encoding": "utf-8",
+        "formatter": "verbose",
+    },
+    # ------------------------------------------------------------------
+    # Celery Beat Handler
+    # ------------------------------------------------------------------
+
+    "celery_beat_file": {
+        "level": "INFO",
+        "class": "logging.handlers.TimedRotatingFileHandler",
+        "filename": LOG_DIR / "celery" / "beat.log",
+        "when": "midnight",
+        "interval": 1,
+        "backupCount": 30,
+        "encoding": "utf-8",
+        "formatter": "verbose",
     },
 }
 
@@ -152,8 +184,41 @@ LOGGING = {
             "level": "INFO",
             "propagate": False,
         },
+        # ------------------------------------------------------------------
+        # Celery Worker Logger
+        # ------------------------------------------------------------------
 
-    },
+        "celery.worker": {
+            "handlers": [
+                "celery_worker_file",
+            ],
+            "level": "INFO",
+            "propagate": False,
+        },
+
+        # ------------------------------------------------------------------
+        # Celery Beat Logger
+        # ------------------------------------------------------------------
+
+        "celery.beat": {
+            "handlers": [
+                "celery_beat_file",
+            ],
+            "level": "INFO",
+            "propagate": False,
+        },
+        # ------------------------------------------------------------------
+        # Celery Worker Logger
+        # ------------------------------------------------------------------
+
+        "celery": {
+            "handlers": [
+                "celery_worker_file",
+                "celery_error_file",
+            ],
+            "level": "INFO",
+            "propagate": False,
+        },
 
     "root": {
         "handlers": ["console"] if LOG_TO_CONSOLE else [],
