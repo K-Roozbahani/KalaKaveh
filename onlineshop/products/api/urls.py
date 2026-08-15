0a028e29252b9710cdaa0623cd9407f0c1cbc16f
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2026-08-15 10:36:55 +0330

    refactor(urls): update review urls

--- onlineshop/products/api/urls.py
+++ onlineshop/products/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from products.api.views.brand import BrandViewSet
@@ -6,12 +6,53 @@ from products.api.views.category import CategoryViewSet
 from products.api.views.product import ProductViewSet
 from products.api.views.review import ReviewViewSet
 
+
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename="category")
-router.register(r'brands', BrandViewSet, basename="brand")
-router.register(r'reviews', ReviewViewSet, basename="review" )
 
-router.register(r'products', ProductViewSet, basename="product")
+router.register(
+    r"categories",
+    CategoryViewSet,
+    basename="category",
+)
 
-urlpatterns = router.urls
+router.register(
+    r"brands",
+    BrandViewSet,
+    basename="brand",
+)
 
+router.register(
+    r"products",
+    ProductViewSet,
+    basename="product",
+)
+
+
+urlpatterns = [
+    *router.urls,
+
+    path(
+
+    "products/<str:product_slug>/reviews/",
+        ReviewViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+        name="product-review-list",
+    ),
+
+    path(
+        "products/<str:product_slug>/reviews/<int:pk>/",
+        ReviewViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="product-review-detail",
+    ),
+]
