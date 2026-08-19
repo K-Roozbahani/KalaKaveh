26280b08bd43d9e6e8f915490d7a1cb6d962e47a
InternetTejarat <kavehrozbah@gmail.com>
2026-08-19 18:51:29 +0330

    feat(urls): add csrf to urls

--- onlineshop/authentication/api/urls.py
+++ onlineshop/authentication/api/urls.py
@@ -1,9 +1,21 @@
 from rest_framework.routers import DefaultRouter
-from authentication.api.views import AuthenticationViewSet
 
+from authentication.api.views.auth import AuthenticationViewSet
+from authentication.api.views.csrf import CSRFTokenView
+
+from django.urls import path
 
 router = DefaultRouter()
 
+
+urlpatterns = [
+    path(
+        "csrf/",
+        CSRFTokenView.as_view(),
+        name="csrf-token",
+    ),
+]
+
 router.register("", AuthenticationViewSet, basename="authentication")
 
-urlpatterns = router.urls
+urlpatterns += router.urls
