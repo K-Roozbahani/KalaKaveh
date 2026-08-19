a3d2ccdae05d975ffbfa5742342fe1a99086b2ff
InternetTejarat <kavehrozbah@gmail.com>
2026-08-19 13:34:05 +0330

    refactor(settings): change DEFAULT_AUTHENTICATION_CLASSES on REST_FRAMEWORK

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -307,7 +307,7 @@ REST_FRAMEWORK = {
     # ------------------------------------------------------
 
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "users.authentication.jwt.CookieJWTAuthentication",
     ),
 
     # ------------------------------------------------------
