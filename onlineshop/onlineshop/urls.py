b037deabc6b445f0a2b1332fe4d6a04d679c41d2
InternetTejarat <kavehrozbah@gmail.com>
2026-08-19 18:22:53 +0330

    feat(urls): add new authentication urls and rm old auth path

--- onlineshop/onlineshop/urls.py
+++ onlineshop/onlineshop/urls.py
@@ -33,8 +33,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('authentication.api.urls')),
     path('api-auth/', include('rest_framework.urls')), # برای لاگین/لاگ‌اوت در مرورگر
     path(
             "api/schema/",
