a5fd3aaea8b634bbac37c5c3c3f5064f590aea88
InternetTejarat <kavehrozbah@gmail.com>
2026-08-19 18:27:03 +0330

    refactor(views): cd from users to authentications

--- onlineshop/authentication/api/views.py
+++ onlineshop/authentication/api/views.py
@@ -1,9 +1,7 @@
-from .serializers import UserSerializer
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
-from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
-from utils.permissions import IsOwnerOrAdmin
+from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
@@ -28,27 +26,6 @@ from authentication.utils.cookies import (
 )
 
 User = get_user_model() # این خط مدل سفارشی شما را به درستی پیدا می‌کند
-
-class UserApiView(ModelViewSet):
-    lookup_field = 'phone_number'
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def get_permissions(self):
-        if self.action == 'create':
-            # اینجا چون داریم لیست رو برمیکردونیم باید از () استفاده شود
-            return [AllowAny(),]
-
-
-        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-            self.permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
-
-        elif self.action == 'list':
-            self.permission_classes = [IsAdminUser,]
-        else:
-            self.permission_classes = [IsAuthenticated,]
-
-        return super().get_permissions()
 
 
 class AuthenticationViewSet(GenericViewSet):
