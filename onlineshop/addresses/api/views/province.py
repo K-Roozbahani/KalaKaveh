025a1cf5d920e7ef288968f2bb2eda02ac5cee4b
InternetTejarat <kavehrozbah@gmail.com>
2026-08-31 14:17:03 +0330

    refactor(views): improve province views

--- onlineshop/addresses/api/views/province.py
+++ onlineshop/addresses/api/views/province.py
@@ -1,19 +1,22 @@
-from rest_framework import mixins, viewsets
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticated
 
 from addresses.api.serializers import (
     ProvinceWithCitiesSerializer,
 )
 from addresses.selectors import get_provinces_with_cities
 
+from utils.api.views import BaseGenericViewSet
+
 
 class ProvinceViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
+    ListModelMixin,
+    BaseGenericViewSet,
 ):
     """
     API دریافت لیست استان‌ها به همراه شهرهای هر استان.
     """
-
+    permission_classes = (IsAuthenticated, )
     serializer_class = ProvinceWithCitiesSerializer
 
     def get_queryset(self):
