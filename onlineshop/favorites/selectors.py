8549793257e8288c2a30179aa3550681dddd8c00
InternetTejarat <kavehrozbah@gmail.com>
2026-09-12 08:51:17 +0330

    feat(selectors): add list_user_favarites on favorits

---
+++ onlineshop/favorites/selectors.py
@@ -0,0 +1,48 @@
+from django.db.models import Prefetch
+
+from favorites.models import Favorite
+from products.models import ProductVariant
+
+
+def list_user_favorites(
+    *,
+    user,
+):
+    """
+    دریافت لیست محصولات مورد علاقه کاربر.
+
+    فقط محصولات و Variantهای فعال دریافت می‌شوند.
+    اطلاعات لازم برای نمایش قیمت نیز از قبل Prefetch می‌شوند.
+    """
+
+    default_variants = (
+        ProductVariant.objects
+        .filter(
+            is_active=True,
+        )
+        .order_by(
+            "final_price",
+            "-stock",
+            "id",
+        )
+    )
+
+    return (
+        Favorite.objects
+        .filter(
+            user=user,
+            product__is_active=True,
+        )
+        .select_related(
+            "product",
+            "product__brand",
+            "product__category",
+        )
+        .prefetch_related(
+            Prefetch(
+                "product__variants",
+                queryset=default_variants,
+            )
+        )
+        .order_by("-created_at")
+    )
