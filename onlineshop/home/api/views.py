892a0837f774b3dca27eab1373dd0cc19fbe28c6
InternetTejarat <kavehrozbah@gmail.com>
2026-09-15 20:15:12 +0330

    fix(views): update home permission_class

--- onlineshop/home/api/views.py
+++ onlineshop/home/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,7 +14,7 @@ class HomePageAPIView(APIView):
     دریافت اطلاعات صفحه اصلی فروشگاه.
     """
 
-    permission_classes = None
+    permission_classes = (AllowAny,)
 
     def get(self, request):
         """
